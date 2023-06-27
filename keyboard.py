5be04c1ae6ed278576fc36a94d8bde0d2dcff177
ReveekJ <sochnevde09@gmail.com>
2023-06-27 10:53:39 +0500

    изменена клавиаура выбора языка

--- keyboard.py
+++ keyboard.py
@@ -2,11 +2,18 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 # callback_data = 'ru' or 'en
-def choose_lang_kb():
-    btn_ru = InlineKeyboardButton(text='Русский', callback_data='ru')
-    btn_en = InlineKeyboardButton(text='English', callback_data='en')
+def choose_lang_kb(lang: str = None):
+    btn_ru = InlineKeyboardButton(text='Изменить на русский 🇷🇺', callback_data='ru')
+    btn_en = InlineKeyboardButton(text='Change to English 🇺🇸', callback_data='en')
 
-    lang_kb = InlineKeyboardMarkup(row_width=1).add(btn_ru, btn_en)
+    if lang == 'ru':
+        btn_cancel = InlineKeyboardButton(text='Не изменять ❌', callback_data='cancel_choose_lang')
+        lang_kb = InlineKeyboardMarkup(row_width=1).add(btn_en, btn_cancel)
+    elif lang == 'en':
+        btn_cancel = InlineKeyboardButton(text='Do not change ❌', callback_data='cancel_choose_lang')
+        lang_kb = InlineKeyboardMarkup(row_width=1).add(btn_ru, btn_cancel)
+    else:
+        btn_cancel = InlineKeyboardButton(text='Не изменять (Do not change) ❌', callback_data='cancel_choose_lang')
+        lang_kb = InlineKeyboardMarkup(row_width=1).add(btn_ru, btn_en, btn_cancel)
 
     return lang_kb
-
