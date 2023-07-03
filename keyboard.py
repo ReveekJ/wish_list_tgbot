7d4140e49ca0c41227f1221ae66e81cc4a5887a5
ReveekJ <sochnevde09@gmail.com>
2023-07-03 22:15:20 +0500

    добавлена клавиатура для выбора пола

--- keyboard.py
+++ keyboard.py
@@ -1,7 +1,8 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from texts import texts
 
 
-# callback_data = 'ru' or 'en
+# callback_data = 'ru' or 'en'
 def choose_lang_kb(lang: str = None):
     btn_ru = InlineKeyboardButton(text='Изменить на русский 🇷🇺', callback_data='ru')
     btn_en = InlineKeyboardButton(text='Change to English 🇺🇸', callback_data='en')
@@ -17,3 +18,13 @@ def choose_lang_kb(lang: str = None):
         lang_kb = InlineKeyboardMarkup(row_width=1).add(btn_ru, btn_en, btn_cancel)
 
     return lang_kb
+
+
+def gender_q_kb(lang: str = None):
+    btn_male = InlineKeyboardButton(text=texts[lang]['male'], callback_data='male')
+    btn_female = InlineKeyboardButton(text=texts[lang]['female'], callback_data='female')
+    btn_none = InlineKeyboardButton(text=texts[lang]['none_gender'], callback_data='none')
+
+    gender_kb = InlineKeyboardMarkup(row_width=1).add(btn_male, btn_female, btn_none)
+
+    return gender_kb
