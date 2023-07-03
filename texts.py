90405af0ad48169926af04e0610fce234b8bfcac
ReveekJ <sochnevde09@gmail.com>
2023-07-03 22:01:45 +0500

    добавлен файл texts.py

---
+++ texts.py
@@ -0,0 +1,10 @@
+texts = {'ru': {'auto_lang': 'Привет! Автоматически выбран русский язык (Hello! Automatically selected Russian '
+                             'language)',
+                'personally_group_name': 'Личное 🔒',
+                'age_q': 'Ваш день рождения',
+                'gender_q': 'Ваш пол: '},
+         'en': {'auto_lang': 'Hello! Automatically selected English language (Привет! Автоматически выбран английский '
+                             'язык)',
+                'personally_group_name': 'Personally 🔒',
+                'age_q': 'Your birthday',
+                'gender_q': "What's your gender:"}}
