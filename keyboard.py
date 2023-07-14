1a1f54bbb8b07ddfc540b697b424a4d9c4e1bb07
ReveekJ <sochnevde09@gmail.com>
2023-07-14 14:43:57 +0500

    добавлена клавиатура выбора месяца

--- keyboard.py
+++ keyboard.py
@@ -29,20 +29,30 @@ def choose_gender_kb(gender: str, lang: str = None):
     btn_none = InlineKeyboardButton(
         text=texts[lang]['none_gender'] if gender != 'none' else texts[lang]['none_gender'] + ' ✅',
         callback_data='none')
+    btn_next = InlineKeyboardButton(text=texts[lang]['next'], callback_data='next')
     # btn_male = InlineKeyboardButton(text='male', callback_data='male')
     # btn_female = InlineKeyboardButton(text='female', callback_data='female')
     # btn_none = InlineKeyboardButton(text='none', callback_data='none')
 
-    gender_kb = InlineKeyboardMarkup(row_width=1).add(btn_male, btn_female, btn_none)
+    gender_kb = InlineKeyboardMarkup(row_width=1).add(btn_male, btn_female, btn_none, btn_next)
 
     return gender_kb
 
 
-def choose_month_birth_kb(lang: str):
-    months = ['dec', 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov' ]
+def choose_month_birth_kb(month: str, lang: str):
+    months = ['dec', 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov']
     month_kb = InlineKeyboardMarkup()
 
-    for i in months:
-        month_kb.add(InlineKeyboardButton(text=texts[lang][i], callback_data=i))
+    # i == 'dec' for example
+    for i in range(0, len(months), 3):
+        month_kb.add(InlineKeyboardButton(text=texts[lang][months[i]] if months[i] != month else texts[lang][months[i]]
+                                          + ' ✅', callback_data=months[i]),
+                     InlineKeyboardButton(text=texts[lang][months[i + 1]] if months[i + 1] != month else
+                     texts[lang][months[i + 1]] + ' ✅', callback_data=months[i + 1]),
+                     InlineKeyboardButton(text=texts[lang][months[i + 2]] if months[i + 2] != month else
+                     texts[lang][months[i + 2]] + ' ✅', callback_data=months[i + 2])
+                     )
+
+    month_kb.add(InlineKeyboardButton(text=texts[lang]['next'], callback_data='next'))
 
     return month_kb
