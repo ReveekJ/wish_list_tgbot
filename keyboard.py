24902d0ebefe13e33a812a359546be408d216a75
ReveekJ <sochnevde09@gmail.com>
2023-07-15 17:10:56 +0500

    оптимизирован choose_lang_kb

--- keyboard.py
+++ keyboard.py
@@ -5,18 +5,16 @@ import db
 
 # callback_data = 'ru' or 'en'
 def choose_lang_kb(lang: str = None):
-    btn_ru = InlineKeyboardButton(text='Изменить на русский 🇷🇺', callback_data='ru')
-    btn_en = InlineKeyboardButton(text='Change to English 🇺🇸', callback_data='en')
+    btn_ru = InlineKeyboardButton(text=texts[lang]['change_to'], callback_data='ru')
+    btn_en = InlineKeyboardButton(text=texts[lang]['change_to'], callback_data='en')
+    btn_next = InlineKeyboardButton(text=texts[lang]['next'], callback_data='next')
 
     if lang == 'ru':
-        btn_cancel = InlineKeyboardButton(text='Не изменять ❌', callback_data='cancel_choose_lang')
-        lang_kb = InlineKeyboardMarkup(row_width=1).add(btn_en, btn_cancel)
+        lang_kb = InlineKeyboardMarkup(row_width=1).add(btn_en, btn_next)
     elif lang == 'en':
-        btn_cancel = InlineKeyboardButton(text='Do not change ❌', callback_data='cancel_choose_lang')
-        lang_kb = InlineKeyboardMarkup(row_width=1).add(btn_ru, btn_cancel)
+        lang_kb = InlineKeyboardMarkup(row_width=1).add(btn_ru, btn_next)
     else:
-        btn_cancel = InlineKeyboardButton(text='Не изменять (Do not change) ❌', callback_data='cancel_choose_lang')
-        lang_kb = InlineKeyboardMarkup(row_width=1).add(btn_ru, btn_en, btn_cancel)
+        lang_kb = InlineKeyboardMarkup(row_width=1).add(btn_ru, btn_en, btn_next)
 
     return lang_kb
 
