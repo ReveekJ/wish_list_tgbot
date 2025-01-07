3b59fc160006b11ef0028bda23881d6ba1906c30
reveek <sochnevde09@gmail.com>
2025-01-07 11:41:49 +0500

    update user model

--- src/db/users/models.py
+++ src/db/users/models.py
@@ -1,15 +1,21 @@
+import datetime
 from typing import Optional
 
-from sqlalchemy import BigInteger
+from sqlalchemy import BigInteger, Enum, Date, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.db.db_connect import Base
+from src.db.users.enums import GenderEnum
 
 
 class UserModel(Base):
     __tablename__ = "users"
 
     id: Mapped[BigInteger] = mapped_column(BigInteger, primary_key=True)
+    birthdate: Mapped[datetime.date] = mapped_column(Date, nullable=True)
+    gender: Mapped[GenderEnum] = mapped_column(Enum(GenderEnum), nullable=True)
+    name: Mapped[Optional[str]]
+    username: Mapped[Optional[str]]
 
     own_wish_lists: Mapped[Optional[list["WishListModel"]]] = relationship(
         back_populates="owner"
--- src/db/users/schemas.py
+++ src/db/users/schemas.py
@@ -1,9 +1,17 @@
+import datetime
+
 from pydantic import BaseModel
 
+from src.db.users.enums import GenderEnum
 
 
 class User(BaseModel):
     id: int
+
+    birthdate: datetime.date
+    gender: GenderEnum
+    name: str
+    username: str
 
     own_wish_lists: list["WishList"]
     member_wish_lists: list["WishList"]
