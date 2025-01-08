84ad35b8b466dff5f37bc7ea808ed22f5b2e5f4c
reveek <sochnevde09@gmail.com>
2025-01-08 11:47:42 +0500

    fix I18NFormat

--- src/custom_widgets/i18n_format.py
+++ src/custom_widgets/i18n_format.py
@@ -2,18 +2,20 @@ from typing import Dict
 
 from aiogram_dialog.api.protocols import DialogManager
 from aiogram_dialog.widgets.common import WhenCondition
-from aiogram_dialog.widgets.text import Text
+from aiogram_dialog.widgets.text import Text, Format
 from fluentogram import TranslatorRunner
 
 
-class I18NFormat(Text):
-    def __init__(self, key: str, when: WhenCondition = None):
+class I18NFormat(Format):
+    def __init__(self, text: str, when: WhenCondition = None):
         super().__init__(when)
-        self.key = key
+        self.text = text
 
     async def _render_text(self, data: Dict, manager: DialogManager) -> str:
+        res = await super()._render_text(data, manager)
+
         i18n: TranslatorRunner = manager.middleware_data.get('i18n')
-        value = i18n.get(self.key, **data)
+        value = i18n.get(res, **data)
         if value is None:
-            raise KeyError(f'translation key = "{self.key}" not found')
+            raise KeyError(f'translation key = "{res}" not found')
         return value
