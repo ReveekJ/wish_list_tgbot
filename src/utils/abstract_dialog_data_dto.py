9259a7dd270da6f6356688cc0804c6311d3a448e
reveek <sochnevde09@gmail.com>
2025-01-08 12:29:22 +0500

    add abstract_dialog_data_dto

---
+++ src/utils/abstract_dialog_data_dto.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import Type
+
+from aiogram_dialog import DialogManager
+from pydantic import BaseModel
+
+
+class DialogDataDTO(ABC):
+    def __init__(self, dialog_manager: DialogManager, data_schema: Type[BaseModel]):
+        self._dialog_manager = dialog_manager
+        self._data_schema = data_schema
+        self._data = self._dialog_manager.dialog_data.get('dto', data_schema.model_validate({}))
+
+    @property
+    @abstractmethod
+    def data(self) -> BaseModel:
+        pass
+
+    @data.setter
+    def data(self, value: BaseModel):
+        if issubclass(value, BaseModel):
+            self._data = value
+        else:
+            raise ValueError(f'{value.__class__.__name__} is not a subclass of BaseModel')
+
+    def save_to_dialog_manager(self, dialog_manager: DialogManager):
+        dialog_manager.dialog_data['dto'] = self.data
+        self._dialog_manager = dialog_manager  # на всякий случай
