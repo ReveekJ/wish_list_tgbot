546280949b7beb5f068cfd0d758f1d926ee37812
reveek <sochnevde09@gmail.com>
2025-01-11 11:08:52 +0500

    bug fix in get_wish_lists_by_user_id

--- src/db/wish_lists/crud.py
+++ src/db/wish_lists/crud.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from sqlalchemy import select
+
 from src.db.wish_lists.models import WishListModel
 from src.db.wish_lists.schemas import WishList
 from src.utils.db_template import DBTemplate, DBParams
@@ -17,7 +19,7 @@ class WishListCRUD(DBTemplate[WishListModel, WishList]):
 
     def get_wish_lists_by_user_id(self, user_id: int) -> List[WishList]:
         try:
-            res = self._db.query(self._model_class).filter(WishListModel.owner_id == user_id).scalar().all()
-            return [self._schema_class.model_validate(i) for i in res]
+            res = self._db.query(self._model_class).filter(WishListModel.owner_id == user_id).all()
+            return [self._schema_class.model_validate(i, from_attributes=True) for i in res]
         except AttributeError:  # возникает, когда нет списков
             return []
