1d8042295ea890d46e06135e7301ce121b6770ac
reveek <sochnevde09@gmail.com>
2025-01-11 21:54:02 +0500

    add dto

---
+++ src/tgbot/my_wish_lists/create_wish_dto.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from aiogram_dialog import DialogManager
+from pydantic import BaseModel
+
+from src.utils.abstract_dialog_data_dto import DialogDataDTO
+
+
+class CreateWishSchema(BaseModel):
+    wish_list_id: Optional[int] = None
+    name: Optional[str] = None
+    photos: list[str] = None
+    description: Optional[str] = None
+    link_to_marketplace: Optional[str] = None
+    price: Optional[int] = None
+
+
+class CreateWishDTO(DialogDataDTO):
+    @property
+    def data(self) -> CreateWishSchema:
+        return self._data
+
+    def __init__(self, dialog_manager: DialogManager):
+        super().__init__(
+            dialog_manager,
+            CreateWishSchema,
+        )
