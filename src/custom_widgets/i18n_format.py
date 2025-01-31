5e50ad86102ab86f934ab340bdc71ad9de00aca8
Daniil Sochnev <sochnevde09@gmail.com>
2025-01-31 21:54:05 +0500

    bug fix

--- src/custom_widgets/i18n_format.py
+++ src/custom_widgets/i18n_format.py
@@ -8,7 +8,7 @@ from fluentogram import TranslatorRunner
 
 class I18NFormat(Format):
     def __init__(self, text: str, when: WhenCondition = None):
-        super().__init__(when)
+        super().__init__(text, when=when)
         self.text = text
 
     async def _render_text(self, data: Dict, manager: DialogManager) -> str:
