38f8a6f67716a0cf273f947e5c267257d8f0ca81
Daniil Sochnev <sochnevde09@gmail.com>
2025-01-31 10:30:59 +0500

    add get_friends_of_user

--- src/db/users/crud.py
+++ src/db/users/crud.py
@@ -1,6 +1,11 @@
+from typing import List
+
+from sqlalchemy import select
+
 from src.db.users.models import UserModel
 from src.db.users.schemas import User
-from src.utils.db_template import DBTemplate, DBParams
+from src.db.wish_lists.schemas import WishList
+from src.utils.db_template import DBTemplate
 
 
 class UserCRUD(DBTemplate[UserModel, User]):
@@ -9,3 +14,15 @@ class UserCRUD(DBTemplate[UserModel, User]):
             model_class=UserModel,
             schema_class=User,
         )
+
+    def get_friends_of_user(self, user_id: int) -> List[User]:
+        query = select(self._model_class).where(self._model_class.id == user_id)
+        res = self._db.execute(query).scalars().first()
+        wish_lists = [WishList.model_validate(i, from_attributes=True) for i in res.member_wish_lists]
+
+        # достаем только уникальных юзеров
+        unique_owner_ids = set()
+        for w in wish_lists:
+            unique_owner_ids.add(w.owner_id)
+
+        return sorted([self.get_obj_by_id(i) for i in unique_owner_ids], key=lambda i: i.name)
